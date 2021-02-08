b69af918c607672cf256abe174db3f2d1adb2734
duzhangbo <1253791508@qq.com>
2021-02-08 21:06:23 +0800

    third post coding hello

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+/*====================================================
+*   Copyright (C) 2021  All rights reserved.
+*
+*   文件名称：hello.c
+*   创 建 者：dzb  1253791508@qq.com
+*   创建日期：2021年02月08日
+*   描    述：
+================================================================*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("hello world!\n");
+    return 0;
+}
